33488fa5970e502d4de77d9d9af5e60d36cffff3
yamamoto hitoki <brumaire1013@gmail.com>
2024-01-14 11:53:11 +0900

    Finish: chapter3

---
+++ chapter3/exercise3.py
@@ -0,0 +1,20 @@
+# print("数値を入力してください >")
+# n = int(input())
+
+# if (n % 2 == 0):
+#     print("偶数") 
+# else:
+#     print("奇数")
+
+# print("文字を入力してください")
+# greeting = str(input())
+
+# if greeting == "こんにちは":
+#     print("ようこそ!")
+# elif greeting == "景気は?":
+#     print("ぼちぼちです")
+# elif greeting == "さようなら":
+#     print("お元気で！")
+# else:
+#     print("どうしました?")
+
